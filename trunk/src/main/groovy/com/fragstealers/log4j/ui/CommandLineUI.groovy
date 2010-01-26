1fb1f53e1eaae9f74cf7087f66808131eb0e8879
jroyals <jroyals@6cec29fc-0423-11df-8469-37713b39fc03>
2010-01-26 07:48:08 +0000

    Break up into some functions

--- trunk/src/main/groovy/com/fragstealers/log4j/ui/CommandLineUI.groovy
+++ trunk/src/main/groovy/com/fragstealers/log4j/ui/CommandLineUI.groovy
@@ -4,13 +4,11 @@ import com.fragstealers.log4j.jaxb.JaxbLog4JPropertiesConverter
 
 class CommandLineUI {
     def showUi(def args) {
-        def cl = new CliBuilder(usage: 'log4j-converter -f "path" [-h] [-v] [-o "dir"] <path to log4j.properties> []')
+        def cl = new CliBuilder(usage: 'log4j-converter [-v] -f <path to log4j.properties>')
 
         cl.h(longOpt: 'help', 'Show usage information and quit')
         cl.f(argName: 'file', longOpt: 'file', args: 1, required: true, 'Source log4j.properties file')
-        cl.o(argName: 'out', longOpt: 'output-directory', args: 1, required: false, 'Output directory where new file will be written. Default is the same directory as the source file.')
-        cl.v(argName: 'verbose', longOpt: 'verbose', args: 1, required: false, 'Prints information to the screen.')
-
+        cl.v(argName: 'verbose', longOpt: 'verbose', required: false, 'Prints information to the screen.')
 
         def opt = cl.parse(args)
 
@@ -25,30 +23,39 @@ class CommandLineUI {
                 println "\nUnable to open file ${opt.f}"
                 cl.usage()
             } else {
-                def log4jProperties = new Properties()
-                try {
-                    log4jProperties.load(source.newInputStream())
-                } catch (Exception e) {
-                    println "\nUnable to parse properties file ${opt.f}, error was ${e.message}"
-                    cl.usage()
-                }
-
-                // -o option should override
-                def destFileName = source.name.replaceAll(/.properties/, ".xml")
-                def dest = new File(source.absoluteFile.parentFile, destFileName)
-
-                if (opt.v) {
-                    println "Using source file ${source.absolutePath} and creating new XML file at ${dest.absolutePath}"
-                }
-
-                def converter = new JaxbLog4JPropertiesConverter()
-                dest.withWriter {writer ->
-                    converter.toXml(log4jProperties, writer)
-                }
-
-                println "Complete, wrote 1 file to ${dest.parent}"
+                doConvert(source, cl, opt)
             }
         }
+    }
+
+    private def doConvert(def source, CliBuilder cl, OptionAccessor opt) {
+        def log4jProperties = loadLog4jProperties(source, cl, opt)
+
+        // -o option should override
+        def destFileName = source.name.replaceAll(/.properties/, ".xml")
+        def dest = new File(source.absoluteFile.parentFile, destFileName)
+
+        if (opt.v) {
+            println "Using source file ${source.absolutePath} and creating new XML file at ${dest.absolutePath}"
+        }
+
+        def converter = new JaxbLog4JPropertiesConverter()
+        dest.withWriter {writer ->
+            converter.toXml(log4jProperties, writer)
+        }
+
+        println "Complete, wrote 1 file to ${dest.parent}"
+    }
+
+    private def loadLog4jProperties(File source, CliBuilder cl, opt) {
+        def log4jProperties = new Properties()
+        try {
+            log4jProperties.load(source.newInputStream())
+        } catch (Exception e) {
+            println "\nUnable to parse properties file ${opt.f}, error was ${e.message}"
+            cl.usage()
+        }
+        return log4jProperties
     }
 
     public static void main(String[] args) {
