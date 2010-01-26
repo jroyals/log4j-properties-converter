c60b3005aab9af2f8c2fb5e43a6c1cab3f865e05
jroyals <jroyals@6cec29fc-0423-11df-8469-37713b39fc03>
2010-01-26 23:46:50 +0000

    Fix issue 1

--- src/main/bin/log4j-convert.bat
+++ src/main/bin/log4j-convert.bat
@@ -10,13 +10,9 @@ set basedir=%basedir:~0,-1%
 if NOT "%removed%"=="\" goto strip
 set LOG4J_CONVERTER_HOME=%basedir%
 
-set TMP_CP="%LOG4J_CONVERTER_HOME%\log4j-converter.jar"
+set TMP_CP="."
 
 dir /b "%LOG4J_CONVERTER_HOME%\lib\*.*" > %TEMP%\log4j-converter-lib.tmp
 FOR /F %%I IN (%TEMP%\log4j-converter-lib.tmp) DO CALL "%LOG4J_CONVERTER_HOME%\addpath.bat" "%LOG4J_CONVERTER_HOME%\lib\%%I"
 
-SET TMP_CP=%TMP_CP%;"%CLASSPATH%"
-SET TMP_PARMS="%LOG4J_CONVERTER_HOME%" %1 %2 %3 %4 %5 %6 %7 %8 %9
-
-@rem Run with no command window. This may not work with older versions of Windows. Use the command above then.
-start "SQuirreL SQL Client" /B "%LOCAL_JAVA%w" -cp %TMP_CP% com.fragstealers.log4j.ui.CommandLineUI %TMP_PARMS%
+"%LOCAL_JAVA%" -cp %TMP_CP% com.fragstealers.log4j.ui.CommandLineUI %1 %2 %3 %4 %5 %6 %7 %8 %9
